462e487d3bd6a06d350baa4e7a15ed56cf93e085
Genesis Vivas <genesis.vivasb@gmail.com>
2023-04-05 09:34:28 -0700

    Add structure and styling to PhotoUpload

--- src/components/PhotoUpload/PhotoUpload.jsx
+++ src/components/PhotoUpload/PhotoUpload.jsx
@@ -1,7 +1,17 @@
 import "./PhotoUpload.scss";
+import dogPic from "../../assets/images/dog-pic.svg";
 
 function PhotoUpload() {
-  return <div></div>;
+  return (
+    <div className="photo-upload">
+      <p className="photo-upload__text">
+        Step 1: Tap below to upload a picture
+      </p>
+      <div className="photo-upload__img-container">
+        <img className="photo-upload__img" src={dogPic} alt="dog" />
+      </div>
+    </div>
+  );
 }
 
 export default PhotoUpload;
--- src/pages/IntakePage/IntakePage.jsx
+++ src/pages/IntakePage/IntakePage.jsx
@@ -2,31 +2,37 @@ import "./IntakePage.scss";
 import arrowLeft from "../../assets/images/icons/chevron-left.svg";
 import closeIcon from "../../assets/images/icons/close-icon.svg";
 import { Link } from "react-router-dom";
+import PhotoUpload from "../../components/PhotoUpload/PhotoUpload";
 
 function IntakePage() {
   return (
-    <div className="intake-header">
-      <Link className="intake-header__link" to="/">
-        <img
-          className="intake-header__link-icon"
-          src={arrowLeft}
-          alt="arrow back"
-        />
-        <p className="intake-header__text">Home</p>
-      </Link>
+    <>
+      <div className="intake-header">
+        <Link className="intake-header__link" to="/">
+          <img
+            className="intake-header__link-icon"
+            src={arrowLeft}
+            alt="arrow back"
+          />
+          <p className="intake-header__text">Home</p>
+        </Link>
 
-      <div className="intake-header__title-wrapper">
-        <p className="intake-header__title">Basic Information</p>
-      </div>
+        <div className="intake-header__title-wrapper">
+          <p className="intake-header__title">Basic Information</p>
+        </div>
 
-      <div className="intake-header__button-wrapper">
-        <img
-          className="intake-header__button"
-          src={closeIcon}
-          alt="close icon"
-        />
+        <div className="intake-header__button-wrapper">
+          <img
+            className="intake-header__button"
+            src={closeIcon}
+            alt="close icon"
+          />
+        </div>
       </div>
-    </div>
+      <div>
+        <PhotoUpload />
+      </div>
+    </>
   );
 }
 
