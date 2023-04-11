866ad4f9d40d2bfb4e1e2bcdc5d88680a23f58fd
genesisvb <123412521+genesisvb@users.noreply.github.com>
2023-04-10 23:03:56 -0700

    Create New Pet Page

---
+++ src/pages/NewPetPage/NewPetPage.jsx
@@ -0,0 +1,12 @@
+import "./NewPetPage.scss";
+import PetForm from "../../components/PetForm/PetForm";
+
+function NewPetPage() {
+  return (
+    <>
+      <PetForm />
+    </>
+  );
+}
+
+export default NewPetPage;
