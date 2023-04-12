665b44f3c626372389d463264d324c12d70009c7
genesisvb <123412521+genesisvb@users.noreply.github.com>
2023-04-11 22:36:46 -0700

    Remove unused imports

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,6 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.scss";
 import LoginPage from "./pages/LoginPage/LoginPage";
 import HomePage from "./pages/HomePage/HomePage";
-import IntakePage from "./pages/IntakePage/IntakePage";
-import ShelterArea from "./components/ShelterArea/ShelterArea";
-import Tasks from "./components/Tasks/Tasks";
-import Review from "./components/Review/Review";
 import NewPetPage from "./pages/NewPetPage/NewPetPage";
 
 function App() {
